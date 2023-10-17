efe0c6aeb71fd056eb224fdb4d87f446f13f483e
mridwanq <137993384+mridwanq@users.noreply.github.com>
2023-10-17 19:05:16 +0700

    up
    up

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '09e266281f65c97aeeab5ca2b77b5981',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
